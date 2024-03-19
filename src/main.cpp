b78a22eb72636d8f8736f8c27d3a2c2c78a6628b
Handabao <18862963156@163.com>
2024-03-19 15:39:08 +0800

    利用 C++11 打包函数  简化代码 这样的话  他就可以 直接提交任意的函数 和 任意的参数了

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "ThreadPool.h"
 
+
 using uLong = unsigned long long;
 
 int sum1(int a, int b) {
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     return a + b;
 }
 
 int sum2(int a, int b, int c) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     return a + b + c;
 }
 
@@ -14,17 +18,20 @@ int main(int argc, char *argv[]) {
 
     ThreadPool pool;
     pool.start(4);
-
+    
     std::future<int> r1 = pool.submitTask(sum1, 1, 2);
-    std::future<int> r1 = pool.submitTask(sum2, 1, 2, 3);
-    std::future<int> r1 = pool.submitTask([](int b, int e) {
+    std::future<int> r2 = pool.submitTask(sum2, 1, 2, 3);
+    std::future<int> r3 = pool.submitTask([](int b, int e) {
         int sum = 0;
         for (int i = b; i < e; ++i) {
             sum += i;
         }
         return sum;
     }, 1, 100);
-    std::cout << r1.get() << std::endl;
 
+    std::cout << "r1: " << r1.get() << std::endl;
+    std::cout << "r2: " << r2.get() << std::endl;
+    std::cout << "r3: " << r3.get() << std::endl;
+    
     getchar();
 }
